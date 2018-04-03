67a9bf252e54e7e2032b3bcae21305be2f428da8
jeffreyworsley <33012187+jeffreyworsley@users.noreply.github.com>
2018-04-02 19:37:51 -0700

    /dev/null compatibility
    this will make it compatible for windows too!!!

--- src/escpos/escpos.py
+++ src/escpos/escpos.py
@@ -21,6 +21,9 @@ import six
 import time
 from re import match as re_match
 
+import os.devnull
+
+
 import barcode
 from barcode.writer import ImageWriter
 
@@ -487,7 +490,10 @@ class Escpos(object):
         barcode_class = barcode.get_barcode_class(barcode_type)
         my_code = barcode_class(data, writer=image_writer)
 
-        my_code.write("/dev/null", {
+        # Make /dev/null for windows too!
+        nullfile = open(os.devnull, "w")
+        
+        my_code.write(nullfile, {
             'module_height': module_height,
             'module_width': module_width,
             'text_distance': text_distance
